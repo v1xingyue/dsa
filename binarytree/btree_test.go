90116e08787809cda6105b2374e0c56981ee11ec
v1xingyue <qixingyue@126.com>
2019-04-25 14:36:37 +0800

    interface{} nil 判断

--- binarytree/btree_test.go
+++ binarytree/btree_test.go
@@ -1,6 +1,7 @@
 package binarytree
 
 import (
+	"dsa/queue"
 	"log"
 	"testing"
 )
@@ -60,4 +61,33 @@ func TestLevelOrder(t *testing.T) {
 
 	LevelLoopTree(root, simplePrint)
 
+}
+
+func TestPushNilNode(t *testing.T) {
+	lq := queue.NewListQueue(32)
+	root := Node(3)
+	t.Log(lq, "\n", root.left, "\n", root.right, "\n", root)
+	lq.Insert(root)
+	lq.Insert(root.left)
+	lq.Insert(root.right)
+	t.Log(lq, "\n", root.left, "\n", root.right, "\n", root)
+
+	a, _ := lq.Shift()
+	b, _ := lq.Shift()
+	c, _ := lq.Shift()
+
+	t.Log("a : ", a)
+	t.Log("b : ", b, b == nil)
+	t.Log("c : ", c, c == nil)
+}
+
+func TestInterfaceNil(t *testing.T) {
+	// 包含两个指针， pt,pv  分别指向 值指针和类型指针
+	// 所以就算赋值为nil ， 那么该 值依然不等于nil
+	// 具体可以参看下边的代码
+	var l interface{}
+	var n *int
+	n = nil
+	l = n
+	t.Log(l == nil)
 }
